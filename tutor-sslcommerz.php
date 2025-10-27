18fa73145922246faffb9d115238b287ee42f49f
Hasin Hayder <hasin@leevio.com>
2025-10-27 20:11:54 +0530

    Added Translation/i18n Support

--- tutor-sslcommerz.php
+++ tutor-sslcommerz.php
@@ -9,7 +9,7 @@
  * License:         GPLv2 or later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     tutor-sslcommerz
- *
+ * Domain Path:    /languages
  */
 
 defined('ABSPATH') || exit;
